23be4bbe9432b704fcd53c2e8ac174ba10b8d527
陈哲 <376811578@qq.com>
2019-03-20 18:47:56 +0800

    Update RNCzNavigationview.m

--- ios/RNCzNavigationview.m
+++ ios/RNCzNavigationview.m
@@ -12,11 +12,11 @@ RCT_EXPORT_METHOD(getTopSpace:(RCTResponseSenderBlock)callback)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
         // iPhone X、XR、XS、XS Max  有安全区域的
-        BOOL isHaveSafeArea = NO;
+        CGFloat bottom = 0;
         if (@available(iOS 11.0, *)) {
-            isHaveSafeArea = [[[[UIApplication sharedApplication] delegate] window] safeAreaInsets].bottom > 0;
+            bottom = [[[[UIApplication sharedApplication] delegate] window] safeAreaInsets].bottom;
         }
-        callback(@[@(isHaveSafeArea ? 22 : 0)]);
+        callback(@[@(bottom - 10)]);
     });
 }
 @end
